900e4f18419d027f726fad003167f21115ff8d4c
Gabrielle Ngo <gabrielletngo@gmail.com>
2020-03-24 20:24:47 -0500

    major html/css changes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// scroll reveal
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {delay: 200});
+sr.reveal('.container', {delay: 650});
+
+// fancybox
 $('[data-fancybox="images"]').fancybox({
   buttons : [
     'slideShow',
