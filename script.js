bf44689a716e87362b7aedacf5e94beca3bcd107
Gabrielle Ngo <gabrielletngo@gmail.com>
2020-03-25 17:48:01 -0500

    photo index page

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// scroll reveal
-window.sr = ScrollReveal();
-sr.reveal('.navbar', {delay: 200});
-sr.reveal('.container', {delay: 750});
-sr.reveal('.container-fluid', {delay: 750});
-
 // photo lightbox
 $(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+// scroll reveal
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {delay: 200});
+sr.reveal('.container', {delay: 750});
+sr.reveal('.container-fluid', {delay: 750});
